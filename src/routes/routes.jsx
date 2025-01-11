bdc75b419112fe581ad6564f3999393ca84bda7a
AlulCode45 <alulgans70@gmail.com>
2025-01-11 08:05:43 +0700

    feat: adding login page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,8 +1,14 @@
 import {createBrowserRouter} from "react-router-dom";
 import Dashboard from "../views/dashboard/dashboard.jsx";
 import DashboardLayout from "../layouts/dashboard.layout.jsx";
+import Login from "../views/auth/login.jsx";
+
 
 const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <Login/>
+    },
     {
         path: '/dashboard',
         element:<DashboardLayout/>,
