695b21c3060ea681b442169a0296c9c3c3099e26
AlulCode45 <alulgans70@gmail.com>
2025-01-11 18:09:12 +0700

    feat: get user by user state

--- src/components/dashboard/topbar.jsx
+++ src/components/dashboard/topbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import {useAuthStore} from "../../store/authStore.js";
 
-function Topbar(props) {
+function Topbar() {
+    const userState = useAuthStore((state) => state.user);
+
     return (
         <div className={'bg-white flex justify-between items-center w-full rounded-md py-3 px-5 mb-5 shadow'}>
             <span className={'font-semibold'}>Dashboard</span>
             <div className="profile flex items-center gap-3">
-                <span>Welcome <span className={'font-semibold'}>Admin</span></span>
+                <span>Welcome <span className={'font-semibold'}>{userState?.name}</span></span>
                 <div className="bg-gray-400 rounded-full w-10 h-10"></div>
             </div>
         </div>
