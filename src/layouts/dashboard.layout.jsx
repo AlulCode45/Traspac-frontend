0d09504d80b9435fb4b3caa9775f9aa6dd1f60d6
AlulCode45 <alulgans70@gmail.com>
2025-01-11 18:09:00 +0700

    feat: validation user logged

--- src/layouts/dashboard.layout.jsx
+++ src/layouts/dashboard.layout.jsx
@@ -1,20 +1,50 @@
-import {Outlet} from "react-router";
+import { Outlet } from "react-router";
 import Sidebar from "../components/dashboard/sidebar.jsx";
 import Topbar from "../components/dashboard/topbar.jsx";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import {useAuthStore} from "../store/authStore.js";
+import {getUserProfile} from "../services/authService.js";
+import toast from "react-hot-toast";
 
+function DashboardLayout() {
+    const navigate = useNavigate();
+    const userState = useAuthStore((state) => state.user);
+    const setUserState = useAuthStore(state => state.setAuthState)
 
-function DashboardLayout(props) {
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            try {
+                const profile = await getUserProfile();
+                if (!profile) {
+                    toast.error("Silahkan login terlebih dahulu!")
+                    navigate("/");
+                }
+                if (!userState) {
+                    console.log(profile)
+                    setUserState({
+                        isLoggedIn: true,
+                        user: profile,
+                    });
+                }
+            } catch (error) {
+                console.error("Failed to fetch user profile:", error);
+            }
+        };
+        fetchUserProfile();
+    }, []);
+
     return (
-        <div className={'bg-gray-100'}>
+        <div className="bg-gray-100 min-h-screen">
             <div className="flex gap-2">
                 <Sidebar />
-                <main className={'p-5 w-4/5'}>
-                    <Topbar/>
-                    <Outlet/>
+                <main className="p-5 flex-1">
+                    <Topbar />
+                    <Outlet />
                 </main>
             </div>
         </div>
