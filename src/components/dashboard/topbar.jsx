5ff225d6d03a887ba1722bbe042b7c97edc5132a
AlulCode45 <alulgans70@gmail.com>
2025-01-11 08:05:22 +0700

    feat: adding shadow

--- src/components/dashboard/topbar.jsx
+++ src/components/dashboard/topbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Topbar(props) {
     return (
-        <div className={'bg-white flex justify-between items-center w-full rounded-md py-3 px-5 mb-5'}>
+        <div className={'bg-white flex justify-between items-center w-full rounded-md py-3 px-5 mb-5 shadow'}>
             <span className={'font-semibold'}>Dashboard</span>
             <div className="profile flex items-center gap-3">
                 <span>Welcome <span className={'font-semibold'}>Admin</span></span>
