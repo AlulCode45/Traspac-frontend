e421e8288a66649b7fea2beddba445ae4a18f55d
AlulCode45 <alulgans70@gmail.com>
2025-01-12 21:14:44 +0700

    feat: add route edit and detail pegawai

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,6 +3,8 @@ import Dashboard from "../views/dashboard/dashboard.jsx";
 import DashboardLayout from "../layouts/dashboard.layout.jsx";
 import Login from "../views/auth/login.jsx";
 import Pegawai from "../views/dashboard/pegawai/pegawai.jsx";
+import DetailPegawai from "../views/dashboard/pegawai/detailPegawai.jsx";
+import EditPegawai from "../views/dashboard/pegawai/editPegawai.jsx";
 
 
 const routes = createBrowserRouter([
@@ -21,6 +23,14 @@ const routes = createBrowserRouter([
             {
                 path: 'pegawai',
                 element: <Pegawai/>,
+            },
+            {
+                path: 'pegawai/:id',
+                element: <DetailPegawai/>,
+            },
+            {
+                path: 'pegawai/:id/edit',
+                element: <EditPegawai/>,
             }
         ]
     }
