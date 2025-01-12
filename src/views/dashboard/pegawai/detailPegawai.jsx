774fb4e86a90c4d6824e44ab310f0dfa6eb92a42
AlulCode45 <alulgans70@gmail.com>
2025-01-12 21:13:58 +0700

    feat: detail pegawai

---
+++ src/views/dashboard/pegawai/detailPegawai.jsx
@@ -0,0 +1,133 @@
+import {useNavigate, useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {getPegawaiById} from "../../../services/pegawaiService.js";
+
+function DetailPegawai() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [pegawai, setPegawai] = useState();
+
+    useEffect(() => {
+        const getDetailPegawai = async () => {
+            return await getPegawaiById(id).then(res => {
+                setPegawai(res.data)
+                console.log(res.data)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+
+        getDetailPegawai()
+    },[id])
+
+
+    return (
+        <div className="bg-white overflow-hidden shadow rounded-lg border">
+            <div className="px-4 py-5 sm:px-6 flex items-center justify-between">
+                <div className="card-title">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">Profil Pegawai</h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                        Informasi detail mengenai pegawai?.
+                    </p>
+                </div>
+                <div className="card-actions">
+                    <button className="bg-blue-500 rounded-md text-white px-5 py-2 font-semibold" onClick={() => navigate(`/dashboard/pegawai/${id}/edit`)} >Edit</button>
+                </div>
+            </div>
+            <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+                <dl className="sm:divide-y sm:divide-gray-200">
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Foto Pegawai</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            <img
+                                src="https://picsum.photos/200/300"
+                                alt="Foto Profil"
+                                className="rounded-md w-full h-32 w-20  block"
+                            />
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">NIP</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.nip}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Nama</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.nama}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Tempat, Tanggal Lahir</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.tempat_lahir}, {pegawai?.tanggal_lahir}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Alamat</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.alamat}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Jenis Kelamin</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.jenis_kelamin}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Golongan</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.golongan}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Eselon</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.eselon}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Jabatan</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.jabatan?.jabatan}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Tempat Tugas</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.tempat_tugas}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Agama</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.agama}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">Unit Kerja</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.unit_kerja?.unit_kerja}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">No HP</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.no_hp}
+                        </dd>
+                    </div>
+                    <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                        <dt className="text-sm font-medium text-gray-500">NPWP</dt>
+                        <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                            {pegawai?.npwp}
+                        </dd>
+                    </div>
+                </dl>
+            </div>
+        </div>
+    );
+}
+
+export default DetailPegawai;
